864338071bdd26b0c4e878bd034844caa43c89be
adapass182 <adampassingham90@gmail.com>
2018-05-29 16:31:20 +0200

    create Game table in database

---
+++ server/src/games/controller.ts
@@ -0,0 +1,52 @@
+import { JsonController, Get, Post, Put, Param, Body, HttpCode, BadRequestError, NotFoundError } from 'routing-controllers'
+import { Game } from './entities'
+// import { randomColor, moves, listOfColors } from './constants'
+// import { validate } from 'class-validator'
+
+@JsonController()
+export default class GameController {
+
+    @Get(`/games`)
+    async getAllGames() {
+        const games = await Game.find()
+        return { games }
+    }
+
+    @Get(`/games/:id`)
+    getGameById(
+        @Param(`id`) id: number
+    ) {
+        return Game.findOne(id)
+    }
+
+    @Post(`/games`)
+    @HttpCode(201)
+    createNewGameWithName(
+        @Body() createGame: Game
+    ) {
+        return createGame.save()
+    }
+
+    // @Put(`/games/:id`)
+    // async editExistingGame(
+    //     @Param(`id`) id: number,
+    //     @Body() update: Partial<Game>
+    // ) {
+    //     const gameToUpdate = await Game.findOne(id)
+    //     if (!gameToUpdate) throw new NotFoundError(`Hi Adam! I'm in games/controller.ts - sorry, I can't find a game with id ${id}!`)
+    //     if (update.color && !listOfColors.includes(update.color)) throw new BadRequestError(`Sorry you can't use ${update.color}, try using one of these instead: ${listOfColors.join(", ")}`)
+    //     if (update.board && moves(gameToUpdate.board, update.board) > 1) {
+    //         throw new BadRequestError(`Ummmm... are you trying to cheat? Only one move at a time please! ` + gameToUpdate.board + update.board)
+    //     }
+    //     const updatedGame = Game.merge(gameToUpdate, update)
+    //     validate(updatedGame).then(errors => {
+    //         if (errors.length > 0) {
+    //             console.log(`Hey Adam, it's games/controller.ts here, I found one or more errors in your PUT endpoint so the validation has failed. Thought you might want to take a look :`, errors);
+    //         } else {
+    //             console.log(`Nailed it!`)
+    //         }
+    //     })
+    //     return updatedGame.save()
+    // }
+
+}
--- server/src/games/entities.ts
+++ server/src/games/entities.ts
@@ -1,20 +1,27 @@
 import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
-import User from '../users/entity'
+// import User from '../users/entity'
+import { IsString } from 'class-validator';
 
-export type Symbol = 'x' | 'o'
-export type Row = [ Symbol | null, Symbol | null, Symbol | null, Symbol | null, Symbol | null, Symbol | null, Symbol | null, Symbol | null, Symbol | null ]
-export type Board = [ Row, Row, Row, Row, Row, Row, Row, Row, Row ]
+export type BoardSymbol =  'o'
+export type PlayerSymbol = 'p1' | 'p2' | 'p3' | 'p4'
+export type Bug = 'bug'
+export type Row = [ BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug, BoardSymbol | PlayerSymbol | Bug ]
+export type Board = [ Row, Row, Row, Row, Row, Row, Row, Row, Row, Row ]
 
 type Status = 'pending' | 'started' | 'finished'
 
-const emptyRow: Row = [null, null, null, null, null, null, null, null, null]
-const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow ]
+const emptyRow: Row = ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o' ]
+const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow, emptyRow ]
 
 @Entity()
 export class Game extends BaseEntity {
 
   @PrimaryGeneratedColumn()
   id?: number
+
+  @IsString()
+  @Column('text', {nullable:false})
+  name: string
 
   @Column('json', {default: emptyBoard})
   board: Board
@@ -27,26 +34,29 @@ export class Game extends BaseEntity {
 
   // this is a relation, read more about them here:
   // http://typeorm.io/#/many-to-one-one-to-many-relations
-  @OneToMany(_ => Player, player => player.game, {eager:true})
-  players: Player[]
+  // @OneToMany(_ => Player, player => player.game, {eager:true})
+  // players: Player[]
 }
 
-@Entity()
-@Index(['game', 'user', 'symbol'], {unique:true})
-export class Player extends BaseEntity {
+// @Entity()
+// @Index(['game', 'user'], {unique:true})
+// export class Player extends BaseEntity {
 
-  @PrimaryGeneratedColumn()
-  id?: number
+//   @PrimaryGeneratedColumn()
+//   id?: number
 
-  @ManyToOne(_ => User, user => user.players)
-  user: User
+//   @ManyToOne(_ => User, user => user.players)
+//   user: User
 
-  @ManyToOne(_ => Game, game => game.players)
-  game: Game
+//   @ManyToOne(_ => Game, game => game.players)
+//   game: Game
 
-  @Column()
-  userId: number
+//   @Column()
+//   userId: number
 
-  @Column('char', {length: 1})
-  symbol: Symbol
-}
+//   @Column('char', {length: 1})
+//   symbol: Symbol
+
+//   @Column('boolean', {default: false, nullable: false})
+//   isDead: Boolean
+// }
